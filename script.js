7e26e595dc3dccf9d5ef50fe47a9bce6b54d0f84
Christian Pfaff <madadaskar@googlemail.com>
2021-07-27 19:02:51 +0200

    Joson array added

---
+++ script.js
@@ -0,0 +1,50 @@
+let questions = [
+  {
+    "question": "Wer hat HTML erfunden?",
+    "answer_1": "Robbie Williams",
+    "answer_2": "Lady Gaga",
+    "answer_3": "Tim Bernes-Lee",
+    "answer-4": "Justin Biber",
+    "right": 3
+  },
+  {
+    "question": "Was bedeutet das THML Tag &lt;a&gt;?",
+    "answer_1": "Text Fett",
+    "answer_2": "Container",
+    "answer_3": "Ein Link",
+    "answer-4": "Kursiv",
+    "right": 3
+  },
+  {
+    "question": "Wie bindet man eine Website in eine Website ein?",
+    "answer_1": "&lt;iframe&gt;,&lt;frame&gt;, and &lt;frameset&gt;",
+    "answer_2": "&lt;iframe&gt;",
+    "answer_3": "&lt;iframe&gt;",
+    "answer-4": "&lt;frameset&gt;",
+    "right": 2
+  },
+  {
+    "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
+    "answer_1": "readonly",
+    "answer_2": "max",
+    "answer_3": "from",
+    "answer-4": "spellcheck",
+    "right": 1
+  },
+  {
+    "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mitdemattribut titleaus?",
+    "answer_1": "a[title]{...}",
+    "answer_2": "a > [title]{...}",
+    "answer_3": "a.[title]{...}",
+    "answer-4": "a=[title]{...}",
+    "right": 1
+  },
+  {
+    "question": "Wie definiert man in JavaScript eine Variable?",
+    "answer_1": "let 100 = rate;",
+    "answer_2": "100 = let rate;",
+    "answer_3": "rate = 100;",
+    "answer-4": "let rate = 100;",
+    "right": 4
+  }
+];
