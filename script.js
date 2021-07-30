801b0f807e108be6d8c90cae11756619ca8b9a60
Christian Pfaff <madadaskar@googlemail.com>
2021-07-30 21:19:10 +0200

    new file added

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+let currentQuestion = 0;
+
+
 let questions = [
   {
     "question": "Wer hat HTML erfunden?",
     "answer_1": "Robbie Williams",
     "answer_2": "Lady Gaga",
     "answer_3": "Tim Bernes-Lee",
-    "answer-4": "Justin Biber",
+    "answer_4": "Justin Biber",
     "right": 3
   },
   {
-    "question": "Was bedeutet das THML Tag &lt;a&gt;?",
+    "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
     "answer_1": "Text Fett",
     "answer_2": "Container",
     "answer_3": "Ein Link",
-    "answer-4": "Kursiv",
+    "answer_4": "Kursiv",
     "right": 3
   },
   {
@@ -20,7 +23,7 @@ let questions = [
     "answer_1": "&lt;iframe&gt;,&lt;frame&gt;, and &lt;frameset&gt;",
     "answer_2": "&lt;iframe&gt;",
     "answer_3": "&lt;iframe&gt;",
-    "answer-4": "&lt;frameset&gt;",
+    "answer_4": "&lt;frameset&gt;",
     "right": 2
   },
   {
@@ -28,7 +31,7 @@ let questions = [
     "answer_1": "readonly",
     "answer_2": "max",
     "answer_3": "from",
-    "answer-4": "spellcheck",
+    "answer_4": "spellcheck",
     "right": 1
   },
   {
@@ -36,7 +39,7 @@ let questions = [
     "answer_1": "a[title]{...}",
     "answer_2": "a > [title]{...}",
     "answer_3": "a.[title]{...}",
-    "answer-4": "a=[title]{...}",
+    "answer_4": "a=[title]{...}",
     "right": 1
   },
   {
@@ -44,7 +47,34 @@ let questions = [
     "answer_1": "let 100 = rate;",
     "answer_2": "100 = let rate;",
     "answer_3": "rate = 100;",
-    "answer-4": "let rate = 100;",
+    "answer_4": "let rate = 100;",
     "right": 4
   }
 ];
+
+function init() {
+  document.getElementById('number-questions').innerHTML = questions.length;
+  showQuestion();
+}
+
+function showQuestion() {
+  let question = questions[currentQuestion];
+  document.getElementById('questiontext').innerHTML = question['question'];
+  document.getElementById('answer_1').innerHTML = question['answer_1'];
+  document.getElementById('answer_2').innerHTML = question['answer_2'];
+  document.getElementById('answer_3').innerHTML = question['answer_3'];
+  document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+//Mein Vorschlag:
+/* function answer(selection) {
+  let question = questions[currentQuestion];
+  let z = question['right'];
+  if (selection == 'answer_' + z) {
+    alert('Richtig!!')
+  }
+  else {
+    alert('Falsch!!')
+  }
+} */
+
