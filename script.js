eb93234c2aaee519b96b492bfcd6ab6653f4360a
Christian Pfaff <madadaskar@googlemail.com>
2021-08-05 17:17:22 +0200

    version 1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let currentQuestion = 0;
-
+let rightQuestions = 0;
+let AUDIO_SUCCESS = new Audio('audio/success.mp3');
+let AUDIO_FAIL = new Audio('audio/fail.mp3');
 
 let questions = [
   {
@@ -16,7 +18,7 @@ let questions = [
     "answer_2": "Container",
     "answer_3": "Ein Link",
     "answer_4": "Kursiv",
-    "right": 3
+    "right_answer": 3
   },
   {
     "question": "Wie bindet man eine Website in eine Website ein?",
@@ -24,7 +26,7 @@ let questions = [
     "answer_2": "&lt;iframe&gt;",
     "answer_3": "&lt;iframe&gt;",
     "answer_4": "&lt;frameset&gt;",
-    "right_anwer": 2
+    "right_answer": 2
   },
   {
     "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
@@ -32,10 +34,10 @@ let questions = [
     "answer_2": "max",
     "answer_3": "from",
     "answer_4": "spellcheck",
-    "right_anwert": 1
+    "right_answer": 1
   },
   {
-    "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mitdemattribut titleaus?",
+    "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut titleaus?",
     "answer_1": "a[title]{...}",
     "answer_2": "a > [title]{...}",
     "answer_3": "a.[title]{...}",
@@ -48,7 +50,7 @@ let questions = [
     "answer_2": "100 = let rate;",
     "answer_3": "rate = 100;",
     "answer_4": "let rate = 100;",
-    "right_anwer": 4
+    "right_answer": 4
   }
 ];
 
@@ -58,14 +60,44 @@ function init() {
 }
 
 function showQuestion() {
+
+  if (gameIsOver()) {
+    showEndscreen();
+  } else {
+    updateProgressBar();
+    updateToNextQuestion();
+  }
+}
+
+function gameIsOver() {
+  return currentQuestion >= questions.length;
+}
+
+function showEndscreen() {
+  document.getElementById('endScreen').style = '';
+  document.getElementById('questionBody').style = 'display: none';
+  document.getElementById('amount-of-questions').innerHTML = questions.length;
+  document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+  document.getElementById('header-image').src = 'img/cup.jpg';
+}
+
+function updateProgressBar() {
+  let percent = (currentQuestion + 1) / questions.length;
+  percent = Math.round(percent * 100);
+  console.log('percent Fortschritt', percent);
+  document.getElementById('progress-bar').innerHTML = `${percent}%`;
+  document.getElementById('progress-bar').style.width = `${percent}%`;
+}
+
+function updateToNextQuestion() {
   let question = questions[currentQuestion];
+  document.getElementById('question-number').innerHTML = currentQuestion + 1;
   document.getElementById('questiontext').innerHTML = question['question'];
   document.getElementById('answer_1').innerHTML = question['answer_1'];
   document.getElementById('answer_2').innerHTML = question['answer_2'];
   document.getElementById('answer_3').innerHTML = question['answer_3'];
   document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
-
 //Mein Vorschlag:
 /* function answer(selection) {
   let question = questions[currentQuestion];
@@ -79,21 +111,56 @@ function showQuestion() {
 } */
 function answer(selection) {
   let question = questions[currentQuestion];
-  console.log('Selected answer is', selection);
   let selectedQuestionNumber = selection.slice(-1);
-  console.log('seletedQuestionNumber is ', selectedQuestionNumber);
-  console.log('Current question is ', question['right_answer']);
-
   let idOfRightAnswer = `answer_${question['right_answer']}`;
 
 
-  if (selectedQuestionNumber == question['right_answer']) {
-    console.log('richtige Antwort!!');
+  if (rightAnswerSelected(selectedQuestionNumber, question)) { //Frage richtig beantwortet
     document.getElementById(selection).parentNode.classList.add('bg-success');
+    rightQuestions++;
+    AUDIO_SUCCESS.play();
   }
-  else {
-    console.log('falsche Antwort!!!');
+  else {//Frage falsch beantwortet
     document.getElementById(selection).parentNode.classList.add('bg-danger');
     document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    AUDIO_FAIL.play();
   }
+  document.getElementById('next-button').disabled = false; //Aktiviert wieder den Button
+}
+
+function rightAnswerSelected(selectedQuestionNumber, question) {
+  return selectedQuestionNumber == question['right_answer'];
+}
+
+function nextQuestion() {
+  currentQuestion++;// z.B.von 0 auf 1
+  //Ativiert wieder den Button
+  document.getElementById('next-button').disabled = true;
+  resetAnswerButtons();
+  showQuestion();
+
+}
+
+function resetAnswerButtons() {
+  //Markierung der Fragen wieder löschen:
+  document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+  document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+  document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+  document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+  document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+  document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+  document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+  document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function restartGame() {
+  document.getElementById('header-image').src = 'img/pencil.jpg';
+  document.getElementById('questionBody').style = '';// questionBody wieder anzeigen
+  document.getElementById('endScreen').style = 'display: none';//Endscreen ausblenden
+
+  currentQuestion = 0;
+  rightQuestions = 0;
+  init();
+
+
 }
